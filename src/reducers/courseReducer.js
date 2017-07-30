f67065ea6f7d6d20875a6bbc1b2fff8f42c13d38
Marcelo <marbarrerach@hotmail.com>
2017-07-30 23:32:17 +0200

    using constants

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * as types from '../actions/actionTypes'
+
 export default function courseReducer(state = [], action){
   switch(action.type){
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
     //debugger; step 2
       return [...state,
         Object.assign({}, action.course)//spread operator to retunr new state
